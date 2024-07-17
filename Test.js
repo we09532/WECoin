471d735ab89f6f908dcf584127395f6e90afbb88
we09532 <163199630+we09532@users.noreply.github.com>
2024-07-16 20:21:10 -0400

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,109 +2,81 @@
 
 // auto arguments is a little over complicated to deduce argument count
 
-const letter = i => String.fromCharCode(97+i)
+const letter = i => String.fromCharCode(97 + i);
 
 const auto_block = (blockType, opcode, text, args) => ({
-	blockType,
-	opcode,
-	text,
-	arguments: Object.fromEntries(
-		new Array(text.split('[').length-1).fill().map((_,i)=> [
-			letter(i), {
-				type: (args && args[i]) || "number", 
-				defaultValue: " "
-			}
-		])
-	)
-})
-const mat_reporter_f = f => o => to_s(f(...new Array(Object.entries(o).length).fill().map((_,i)=> from_s(o[letter(i)]))))
+    blockType,
+    opcode,
+    text,
+    arguments: Object.fromEntries(
+        new Array(text.split('[').length - 1).fill().map((_, i) => [
+            letter(i), {
+                type: (args && args[i]) || "number",
+                defaultValue: " "
+            }
+        ])
+    )
+});
+
+const mat_reporter_f = f => o => to_s(f(...new Array(Object.entries(o).length).fill().map((_, i) => from_s(o[letter(i)])));
 
 class ScratchMath {
 
-	constructor(runtime) {
-		this.runtime = runtime
-	/*	this.tempElem = document.createElement("link")
-			this.tempElem.rel = "stylesheet"
-			this.tempElem.type = "text/css"
-			this.tempElem.href = "https://scratchjs.crossscar.repl.co/styles.css"
-			document.head.appendChild(this.tempElem)
-			
-			this.consoleElem = document.createElement("div")
-			this.consoleElems = {}
-			this.consoleElem.classList.add("ScratchJS-console")
-			this.consoleElems.clearBtn = document.createElement("button")
-			this.consoleElems.clearBtn.classList.add("ScratchJS-console-clear")
-			this.consoleElems.clearBtn.innerHTML = "X"
-			this.consoleElems.clearBtn.title = "Clear Console"
-			this.consoleElem.appendChild(this.consoleElems.clearBtn)
-			document.querySelector(".stage-wrapper_stage-wrapper_2bejr").appendChild(this.consoleElem)
+    constructor(runtime) {
+        this.runtime = runtime;
+    }
 
-			this.consoleElems.clearBtn.addEventListener("click", () => {
-				this.console.clear()
-			}) */
-	}
+    getInfo() {
+        return {
+            id: "WECTE",
+            name: "WE Coin Tools Extension",
+            blocks: [
+                auto_block('reporter', "Fetch", "Fetch [a]"),
+                auto_block('boolean', "test", "test [a]"),
+                {
+                    blockType: 'command',
+                    opcode: 'EvalCmd',
+                    text: 'Run [a] in Javascript',
+                    arguments: {
+                        a: {
+                            type: "string",
+                            defaultValue: " "
+                        }
+                    }
+                },
 
-	getInfo() {
-	    return {
-	    	id: "WECTE",
-	    	name: "WE Coin Tools Extension",
-	    	blocks: [
-		auto_block('reporter', "Fetch", "Fetch [a]"),
-		auto_block('boolean', "test", "test [a]"),
-	        {
-	        	blockType: 'command',
-	        	opcode: 'EvalCmd',
-	        	text: 'Run [a] in Javascript',
-	        	arguments: {
-	        		a: {
-	        			type: "string",
-	        			defaultValue: " "
-	        		},
-	        		b: {
-	        			type: "string",
-	        			defaultValue:" "
-	        		}
-	        	}
-	        },
+                '---',
+            ]
+        };
+    }
 
-	        '---',
+    EvalCmd({ a }) {
+        try {
+            eval(a);
+        } catch (err) {
+            alert(err);
+        }
+    }
 
-	        
-	    	],
-	    	
-	    }
-	}
+    Fetch({ a }) {
+        let file = a;
+        return fetch(file)
+            .then(x => x.text())
+            .then(y => {
+                let ans = y;
+                return ans;
+            });
+    }
 
-	EvalCmd({a}) {
-		try {
-eval(a)
-  
-
-} catch (err) {
-
-  alert(err)
-
-}
-	}
-
-	Fetch({a}) {
-    let file = a;
-    return fetch(file)
-        .then(x => x.text())
-        .then(y => {
-            let ans = y;
-            return ans;
-        });
-	test({a}) {
-    let file = a;
-    return fetch(file)
-        .then(x => x.text())
-        .then(y => {
-            let ans = y;
-            return "true";
-        });
-}
-	
+    test({ a }) {
+        let file = a;
+        return fetch(file)
+            .then(x => x.text())
+            .then(y => {
+                let ans = y;
+                return "true";
+            });
+    }
 }
 
 // ============== globalize vm and load extension ===============
@@ -121,7 +93,7 @@ function findReactComponent(element) {
         return parent;
     };
     fiber = go(fiber);
-    while(fiber.stateNode == null) {
+    while (fiber.stateNode == null) {
         fiber = go(fiber);
     }
     return fiber.stateNode;
@@ -129,8 +101,8 @@ function findReactComponent(element) {
 
 window.vm = findReactComponent(document.getElementsByClassName("stage-header_stage-size-row_14N65")[0]).props.vm;
 
-(function() {
-    var extensionInstance = new ScratchMath(window.vm.extensionManager.runtime)
-    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance)
-    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName)
-})()
+(function () {
+    var extensionInstance = new ScratchMath(window.vm.extensionManager.runtime);
+    var serviceName = window.vm.extensionManager._registerInternalExtension(extensionInstance);
+    window.vm.extensionManager._loadedExtensions.set(extensionInstance.getInfo().id, serviceName);
+})();
